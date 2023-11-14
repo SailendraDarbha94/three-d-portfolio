a1b3ba4eb8b0e51e5201784161b451d868024c12
Sailendra Darbha <sailendra.darbha@gmail.com>
2023-11-14 20:10:30 +0530

    contact updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -66,73 +66,78 @@ const Contact = () => {
   };
 
   return (
-    <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
-    >
-      <motion.div
-        variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
-      >
-        <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact.</h3>
+    // <div
+    //   className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    // >
+    //   <motion.div
+    //     variants={slideIn("left", "tween", 0.2, 1)}
+    //     className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+    //   >
+    //     <p className={styles.sectionSubText}>Get in touch</p>
+    //     <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
-        >
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
-            <input
-              type='text'
-              name='name'
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
-            <input
-              type='email'
-              name='email'
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
-          <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
-            <textarea
-              rows={7}
-              name='message'
-              value={form.message}
-              onChange={handleChange}
-              placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
-            />
-          </label>
+    //     <form
+    //       ref={formRef}
+    //       onSubmit={handleSubmit}
+    //       className='mt-12 flex flex-col gap-8'
+    //     >
+    //       <label className='flex flex-col'>
+    //         <span className='text-white font-medium mb-4'>Your Name</span>
+    //         <input
+    //           type='text'
+    //           name='name'
+    //           value={form.name}
+    //           onChange={handleChange}
+    //           placeholder="What's your good name?"
+    //           className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+    //         />
+    //       </label>
+    //       <label className='flex flex-col'>
+    //         <span className='text-white font-medium mb-4'>Your email</span>
+    //         <input
+    //           type='email'
+    //           name='email'
+    //           value={form.email}
+    //           onChange={handleChange}
+    //           placeholder="What's your web address?"
+    //           className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+    //         />
+    //       </label>
+    //       <label className='flex flex-col'>
+    //         <span className='text-white font-medium mb-4'>Your Message</span>
+    //         <textarea
+    //           rows={7}
+    //           name='message'
+    //           value={form.message}
+    //           onChange={handleChange}
+    //           placeholder='What you want to say?'
+    //           className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+    //         />
+    //       </label>
 
-          <button
-            type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
-      </motion.div>
+    //       <button
+    //         type='submit'
+    //         className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+    //       >
+    //         {loading ? "Sending..." : "Send"}
+    //       </button>
+    //     </form>
+    //   </motion.div>
 
-      <motion.div
-        variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
-      >
-        <div className="h-full w-full flex justify-center items-center">
-          <img src={po} alt="profile" className="w-full rounded-lg" />
-        </div>
-        {/* <EarthCanvas /> */}
-      </motion.div>
+    //   <motion.div
+    //     variants={slideIn("right", "tween", 0.2, 1)}
+    //     className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+    //   >
+    //     <div className="h-full w-full flex justify-center items-center">
+    //       <img src={po} alt="profile" className="w-full rounded-lg" />
+    //     </div>
+    //     {/* <EarthCanvas /> */}
+    //   </motion.div>
+    // </div>
+    <div className="flex justify-center items-center">
+
+      <a href="www.linkedin.com/in/sailendra-darbha" target="blank" className="m-2 p-2 hover:text-white hover:bg-black bg-blue-400 rounded-md text-black">LinkedIn</a>
+      <a href="https://github.com/SailendraDarbha94" target="blank" className="m-2 p-2 hover:text-white hover:bg-black bg-blue-400 rounded-md text-black">GitHub</a>
     </div>
   );
 };
